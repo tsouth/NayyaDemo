0aae879f5f0aaf45fb96596c279b61463cd42957
Thomas Southworth <thomas@mindrglobal.com>
2021-08-09 15:57:46 -0400

    Create Email tests for calls to action

--- src/test/java/com/mindr/tests/admin/AdminCallToActionTests.java
+++ src/test/java/com/mindr/tests/admin/AdminCallToActionTests.java
@@ -1,22 +1,32 @@
 package com.mindr.tests.admin;
 
+import com.mindr.pages.calltoactionpage.CallToActionPage;
 import com.mindr.pages.communitypages.callstoactionpages.ActiveCallsToActionTab;
 import com.mindr.pages.communitypages.callstoactionpages.NewCallToActionPage;
 import com.mindr.pages.communitypages.callstoactionpages.PublishCallToActionConfirmationModal;
 import com.mindr.pages.communitypages.callstoactionpages.UploadCallToActionPhotoModal;
 import com.mindr.pages.communitypages.eventspages.UpcomingEventsTab;
 import com.mindr.pages.dashboardpages.communitymanagementpage.ActiveCommunitiesTab;
+import com.mindr.pages.eventpage.EventPage;
 import com.mindr.pages.homepage.MyDashboardTab;
 import com.mindr.pages.loginpage.LoginPage;
+import com.mindr.utilities.email.EmailUtility;
 import com.mindr.utilities.image.ImageUtility;
 import com.mindr.utilities.managers.PageManager;
 import com.mindr.utilities.testcase.RetryAnalyzer;
+import com.mindr.utilities.testcase.TestCase;
+import org.testng.TestException;
 import org.testng.annotations.*;
 
-public class AdminCallToActionTests {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AdminCallToActionTests implements TestCase {
     private String testImagePath;
     private final ImageUtility imageUtility = new ImageUtility();
+    private final EmailUtility emailUtility = new EmailUtility();
 
+    @Override
     @Parameters({"environment"})
     @BeforeMethod
     public void setup(@Optional("production") String environment) {
@@ -38,6 +48,31 @@ public class AdminCallToActionTests {
         PublishCallToActionConfirmationModal publishCallToActionConfirmationModal = newCallToActionPage.submitCallToActionDetails();
         publishCallToActionConfirmationModal.publishCallToAction();
         activeCallsToActionTab.verifyCallToActionCreated();
+
+        String email = "";
+
+        for (int i = 0; i < emailUtility.getRetryLimit(); i++) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignore) {}
+
+            email = emailUtility.getEmail("Inbox", "Selenium Testing Call to Action");
+            if (email != null && !email.equals("")) {
+                break;
+            }
+        }
+
+        Matcher urlIds = Pattern.compile("ls/click([^/]+?(?=\"))").matcher(email);
+        String urlId;
+        if (urlIds.find()) {
+            urlId = urlIds.group(1);
+        } else {
+            throw new TestException("Register For Call to Action URL Not Found");
+        }
+
+        CallToActionPage callToActionPage = PageManager.getInstance().navigateToPage(CallToActionPage.class, urlId);
+        callToActionPage.verifyCallToActionEmailCreated();
+
     }
 
     @AfterMethod
